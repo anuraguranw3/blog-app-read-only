feb67a074bcf369bcb76a2fe7a1bb4e847e270a3
Anurag Uranw <skanuraguranw12@gmail.com>
2025-05-04 16:44:08 +0530

    update readme

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": "https://blog-app-backend-ri4x.onrender.com",
     }
   },
   plugins: [react()],
